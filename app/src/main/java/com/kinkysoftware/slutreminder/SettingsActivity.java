288f4f4771cf6aaa3257f60ce9c1606818265245
jragsdale <jragsdale@agrisk.net>
2019-01-02 19:57:18 -0600

    Implemented options for the various frequencies of reminders.

--- app/src/main/java/com/kinkysoftware/slutreminder/SettingsActivity.java
+++ app/src/main/java/com/kinkysoftware/slutreminder/SettingsActivity.java
@@ -3,7 +3,6 @@ package com.kinkysoftware.slutreminder;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -21,6 +20,7 @@ import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
 import android.text.TextUtils;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import java.util.List;
 
@@ -114,10 +114,11 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static class ReminderFrequencyPreferenceFragment extends PreferenceFragment {
 
-        PreferenceCategory dailyOptions;
-        PreferenceCategory weeklyOptions;
-        PreferenceCategory monthlyOptions;
-        PreferenceCategory customOptions;
+        TimePreference dailyTime;
+        ListPreference weeklyDayOfWeek;
+        TimePreference weeklyTimeOfDay;
+        ListPreference monthlyDayOfMonth;
+        TimePreference monthlyTimeOfDay;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -128,19 +129,44 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             final PreferenceScreen screen = getPreferenceScreen();
             final ListPreference frequency = (ListPreference)findPreference("reminder_frequency");
 
-            dailyOptions = (PreferenceCategory)findPreference("pref_reminder_daily_category");
-            weeklyOptions = (PreferenceCategory)findPreference("pref_reminder_weekly_category");
-            monthlyOptions = (PreferenceCategory)findPreference("pref_reminder_monthly_category");
-            customOptions = (PreferenceCategory)findPreference("pref_reminder_custom_category");
+            dailyTime = (TimePreference)findPreference("pref_frequency_daily_time");
+            weeklyDayOfWeek = (ListPreference)findPreference("pref_frequency_weekly_day_of_week");
+            weeklyTimeOfDay = (TimePreference)findPreference("pref_frequency_weekly_time_of_day");
+            monthlyDayOfMonth = (ListPreference)findPreference("pref_frequency_monthly_day_of_month");
+            monthlyTimeOfDay = (TimePreference)findPreference("pref_frequency_monthly_time_of_day");
 
+            weeklyDayOfWeek.setSummary(weeklyDayOfWeek.getValue());
+            weeklyTimeOfDay.setTitle("Every " + weeklyDayOfWeek.getValue() + " At");
+            monthlyDayOfMonth.setSummary("On the " + monthlyDayOfMonth.getValue() + " Day of the Month");
+            monthlyTimeOfDay.setTitle("Every " + monthlyDayOfMonth.getValue() + " Day of the Month At");
             setFrequencySummaryAndShowOptions(screen, frequency, frequency.getValue());
 
-            frequency.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                                                               public boolean onPreferenceChange(Preference preference, Object newValue) {
-                                                                   setFrequencySummaryAndShowOptions(screen, frequency, newValue.toString());
-                                                                   return true;
-                                                               }
-                                                           });
+            Preference.OnPreferenceChangeListener onFrequencyChangeListener = new Preference.OnPreferenceChangeListener() {
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    setFrequencySummaryAndShowOptions(screen, frequency, newValue.toString());
+                    return true;
+                }
+            };
+
+            Preference.OnPreferenceChangeListener onDayOfWeekChangeListener = new Preference.OnPreferenceChangeListener() {
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    weeklyDayOfWeek.setSummary(newValue.toString());
+                    weeklyTimeOfDay.setTitle("Every " + newValue.toString() + " At");
+                    return true;
+                }
+            };
+
+            Preference.OnPreferenceChangeListener onDayOfMonthChangeListener = new Preference.OnPreferenceChangeListener() {
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    monthlyDayOfMonth.setSummary("On the " + newValue.toString() + " Day of the Month");
+                    monthlyTimeOfDay.setTitle("Every " + newValue.toString() + " Day of the Month At");
+                    return true;
+                }
+            };
+
+            frequency.setOnPreferenceChangeListener(onFrequencyChangeListener);
+            weeklyDayOfWeek.setOnPreferenceChangeListener(onDayOfWeekChangeListener);
+            monthlyDayOfMonth.setOnPreferenceChangeListener(onDayOfMonthChangeListener);
         }
 
         private void setFrequencySummaryAndShowOptions(PreferenceScreen screen, Preference preference, String value) {
@@ -151,20 +177,18 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             switch (value) {
                 case "Daily":
                     screen.addPreference(preference);
-                    screen.addPreference(dailyOptions);
+                    screen.addPreference(dailyTime);
                     break;
                 case "Weekly":
                     screen.addPreference(preference);
-                    screen.addPreference(weeklyOptions);
+                    preference.setSummary(value);
+                    screen.addPreference(weeklyDayOfWeek);
+                    screen.addPreference(weeklyTimeOfDay);
                     break;
                 case "Monthly":
                     screen.addPreference(preference);
-                    screen.addPreference(monthlyOptions
-                    );
-                    break;
-                case "Custom":
-                    screen.addPreference(preference);
-                    screen.addPreference(customOptions);
+                    screen.addPreference(monthlyDayOfMonth);
+                    screen.addPreference(monthlyTimeOfDay);
                     break;
                 case "Never":
                     screen.addPreference(preference);
